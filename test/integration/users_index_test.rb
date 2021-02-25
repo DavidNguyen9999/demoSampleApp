75ea346639601a2ac85b641d24b9245afc14fee5
DavidNguyen9999 <haianh13041999@gmail.com>
2021-02-25 22:53:33 +0700

    Add account activation

--- test/integration/users_index_test.rb
+++ test/integration/users_index_test.rb
@@ -8,12 +8,14 @@ class UsersIndexTest < ActionDispatch::IntegrationTest
 
   test "index as admin including pagination and delete links" do
     log_in_as(@admin)
+    first_page_of_users = User.paginate(page: 1)
+    first_page_of_users.first.toggle!(:activated)
     get users_path
     assert_template "users/index"
     assert_select "div.pagination"
-    first_page_of_users = User.paginate(page: 1)
-    first_page_of_users.each do |user|
-      assert_select "a[href=?]", user_path(user), text: user.name
+    assigns(:users).each do |user|
+      assert user.FILL_IN
+      assert_select 'a[href=?]', user_path(user), text: user.name
       unless user == @admin
         assert_select "a[href=?]", user_path(user), text: "delete"
       end
@@ -22,6 +24,7 @@ class UsersIndexTest < ActionDispatch::IntegrationTest
       delete user_path(@non_admin)
     end
   end
+  
   test "index as non-admin" do
     log_in_as(@non_admin)
     get users_path
