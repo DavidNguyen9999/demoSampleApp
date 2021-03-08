463198bfab2ba37e0b1bf9e1083299b6d6f73aab
DavidNguyen9999 <haianh13041999@gmail.com>
2021-03-08 11:38:44 +0700

    refs #1 fix bug rubocop

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,6 @@
+# frozen_string_literal: true
+
+# Class ApplicationController
 class ApplicationController < ActionController::Base
   #  include SessionsHelper
   #
@@ -17,8 +20,12 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :email, :password, :remember_me, :password_confirmation) }
+    devise_parameter_sanitizer.permit(:sign_up) do |u|
+      u.permit(:name, :email, :password, :remember_me, :password_confirmation)
+    end
     devise_parameter_sanitizer.permit(:sign_in) { |u| u.permit(:name, :email, :password, :remember_me) }
-    devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:name, :email, :password, :password_confirmation, :current_password) }
+    devise_parameter_sanitizer.permit(:account_update) do |u|
+      u.permit(:name, :email, :password, :password_confirmation, :current_password)
+    end
   end
 end
--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -1,29 +1,46 @@
-class OmniauthCallbacksController < Devise::OmniauthCallbacksController
-  def google_oauth2
-    @user = User.find_for_google_oauth2(request.env["omniauth.auth"].provider, request.env["omniauth.auth"].uid, request.env["omniauth.auth"].extra.raw_info.name, request.env["omniauth.auth"].info.email, request.env["omniauth.auth"].info.image, current_user)
+# frozen_string_literal: true
 
-    if @user.persisted?
-      flash[:notice] = I18n.t "devise.omniauth_callbacks.success", :kind => "Google"
+# Class OmniauthCallbacksController
+class OmniauthCallbacksController < Devise::OmniauthCallbacksController
+  before_action :fetch_user_oauth2, only: :google_oauth2
+  before_action :fetch_user_oauth, only: :facebook
+  def user_google_persisted?(user)
+    if user.persisted?
+      flash[:notice] = I18n.t 'devise.omniauth_callbacks.success', kind: 'Google'
 
       sign_in_and_redirect @user, event: :authentication
     else
-      session["devise.google_data"] = request.env["omniauth.auth"].except(:extra)
+      session['devise.google_data'] = request.env['omniauth.auth'].except(:extra)
 
       redirect_to new_user_registration_url
     end
   end
 
-  def facebook
-    @user = User.find_for_facebook_oauth(request.env["omniauth.auth"].provider, request.env["omniauth.auth"].uid, request.env["omniauth.auth"].extra.raw_info.name, request.env["omniauth.auth"].info.email, request.env["omniauth.auth"].info.image, current_user)
-
-    if @user.persisted?
-      flash[:notice] = I18n.t "devise.omniauth_callbacks.success", :kind => "Facebook"
+  def user_facebook_persisted?(user)
+    if user.persisted?
+      flash[:notice] = I18n.t 'devise.omniauth_callbacks.success', kind: 'Facebook'
 
       sign_in_and_redirect @user, event: :authentication
     else
-      session["devise.facebook_data"] = request.env["omniauth.auth"].except(:extra)
+      session['devise.facebook_data'] = request.env['omniauth.auth'].except(:extra)
 
       redirect_to new_user_registration_url
     end
+  end
+
+  def fetch_user_oauth2
+    @user = User.find_for_google_oauth2(request.env['omniauth.auth'])
+  end
+
+  def fetch_user_oauth
+    @user = User.find_for_facebook_oauth(request.env['omniauth.auth'])
+  end
+
+  def facebook
+    user_facebook_persisted?(@user)
+  end
+
+  def google_oauth2
+    user_google_persisted?(@user)
   end
 end
