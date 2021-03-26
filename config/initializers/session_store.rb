4caaa7a847ec29c598cf7ab9a83258f0b7badaa4
DavidNguyen9999 <haianh13041999@gmail.com>
2021-03-26 08:45:53 +0700

    refs #3 Circle CI Rubocop Heroku 2

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,1 +1,1 @@
-Rails.application.config.session_store :cookie_store, key: "_demoSampleApp_session_#{Rails.env}", domain: :all
+Rails.application.config.session_store :cookie_store, key: "_demoSampleApp_session_#{Rails.env}"
