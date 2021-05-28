28360b4e9904d4f0205c523517da89b10e5a6944
DavidNguyen9999 <haianh13041999@gmail.com>
2021-05-28 14:02:33 +0700

    refs #23 fixx

--- app/services/export_csv_service.rb
+++ app/services/export_csv_service.rb
@@ -2,25 +2,25 @@ require 'csv'
 
 class ExportCsvService
   def initialize current_user
-    @objects = current_user
+    @user = current_user
   end
 
   def microposts
-    @data = @objects.microposts.last_month
+    @data = @user.microposts.last_month
     @attributes = Micropost::CSV_ATTRIBUTES
     @header = Micropost::CSV_ATTRIBUTES.map { |attr| I18n.t("header_csv.#{attr}") }
     perform
   end
 
   def following
-    @data = @objects.following_last_month
+    @data = @user.following_last_month
     @attributes = User::CSV_ATTRIBUTES
     @header = User::CSV_ATTRIBUTES.map { |attr| I18n.t("header_csv.#{attr}") }
     perform
   end
 
   def followed
-    @data = @objects.followed_last_month
+    @data = @user.followed_last_month
     @attributes = User::CSV_ATTRIBUTES
     @header = User::CSV_ATTRIBUTES.map { |attr| I18n.t("header_csv.#{attr}") }
     perform
